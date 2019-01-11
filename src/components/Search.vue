74a976176514a9eac41626e990299dc16cc6dba7
Siemion Juszkiewicz <ms.accaunt@yandex.ru>
2019-01-11 17:34:09 +0700

    Created markup and basic styles

---
+++ src/components/Search.vue
@@ -0,0 +1,54 @@
+<template lang='pug'>
+  form.explorer
+    input.search(type='search' placeholder='Enter .onion adress or search request' autofocus)
+    input.submit(type='submit' value='')
+</template>
+
+<script>
+export default {
+  name: 'Search'
+}
+</script>
+
+<style scoped lang="scss">
+
+.explorer {
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid lightgrey;
+
+  @media screen and (max-width: 400px) {
+    width: 90%;
+  }
+
+  .search, .submit {
+    height: 40px;
+    border: none;
+  }
+
+  .search {
+    width: 400px;
+    padding-left: 10px;
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  .submit {
+    width: fit-content;
+    height: fit-content;
+    outline: none;
+    padding: 5px;
+    margin: 0 10px;
+    right: 10px;
+    opacity: .7;
+    background: url('../assets/search.svg') no-repeat center center;
+    background-color: transparent;
+  }
+
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang='pug'>
+  .home
+    h1.title Torland's Gate
+    Search
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Search from '@/components/Search.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Search
   }
 }
 </script>
+
+<style lang="scss">
+
+.home {
+
+  margin-top: -60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media screen and (max-width: 400px) {
+    width: 100%;
+  }
+
+  .title {
+    margin-top: 0;
+    font-size: 3em;
+    user-select: none;
+
+    @media screen and (max-width: 400px) {
+      font-size: 2em;
+    }
+  }
+
+}
+
+</style>
