44341dcb038470a81f02d1ab8adfc00331576b27
Siemion Juszkiewicz <ms.accaunt@yandex.ru>
2019-01-13 13:40:28 +0700

    Included vue-lodash library

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import router from './router'
 // Normalize.css
 import normalize from 'normalize.css'
 
+// vue-lodash
+import VueLodash from 'vue-lodash'
+Vue.use(VueLodash)
+
 Vue.config.productionTip = false
 
 new Vue({
